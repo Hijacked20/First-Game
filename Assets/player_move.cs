4ef7f8a9da9caed9ec03bd693036ac3ebfe598f8
Hijacked20 <44483090+Hijacked20@users.noreply.github.com>
2018-10-26 17:15:20 +1000

    Added all of the unity files
    Sorry WiFi network! Probably should have used a gitignore

---
+++ Assets/player_move.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class player_move : MonoBehaviour {
+    public Rigidbody Rb;
+    // Use this for initialization
+    void Start()
+    {
+        Rigidbody.AddForce(0, 200, 500);
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
